51f114acfc026b46f455e00c69d76f1d17b25481
ahmed14527 <108636071+ahmed14527@users.noreply.github.com>
2023-10-30 21:34:25 +0200

    mod

--- Assignment/urls.py
+++ Assignment/urls.py
@@ -1,14 +1,31 @@
-from django.urls import include, path
+from django.urls import path
 from rest_framework import routers
-from Assignment.views import  MCQAssignmentViewSet, MCQQuestionViewSet, OptionViewSet, MCQSubmissionViewSet, MCQAnswerViewSet
+from .views import (
+    MCQAssignmentViewSet,
+    MCQAssignmentListAPIView,
+    MCQQuestionViewSet,
+    MCQQuestionListAPIView,
+    OptionViewSet,
+    OptionListAPIView,
+    MCQSubmissionViewSet,
+    MCQSubmissionListAPIView,
+    MCQAnswerViewSet,
+    MCQAnswerListAPIView,
+)
 
 router = routers.DefaultRouter()
-router.register(r'assignments', MCQAssignmentViewSet)
-router.register(r'questions', MCQQuestionViewSet)
-router.register(r'options', OptionViewSet)
-router.register(r'submissions', MCQSubmissionViewSet)
-router.register(r'answers', MCQAnswerViewSet)
+router.register('create-mcq-assignments', MCQAssignmentViewSet)
+router.register('create-mcq-questions', MCQQuestionViewSet)
+router.register('create-options', OptionViewSet)
+router.register('create-mcq-submissions', MCQSubmissionViewSet)
+router.register('create-mcq-answers', MCQAnswerViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('mcq-assignments/', MCQAssignmentListAPIView.as_view(), name='mcq-assignment-list'),
+    path('mcq-questions/', MCQQuestionListAPIView.as_view(), name='mcq-question-list'),
+    path('options/', OptionListAPIView.as_view(), name='option-list'),
+    path('mcq-submissions/', MCQSubmissionListAPIView.as_view(), name='mcq-submission-list'),
+    path('mcq-answers/', MCQAnswerListAPIView.as_view(), name='mcq-answer-list'),
 ]
+
+urlpatterns += router.urls
--- Assignment/views.py
+++ Assignment/views.py
@@ -1,25 +1,63 @@
 from rest_framework import viewsets
 from .models import  MCQAssignment, MCQQuestion, Option, MCQSubmission, MCQAnswer
 from .serializers import  MCQAssignmentSerializer, MCQQuestionSerializer, OptionSerializer, MCQSubmissionSerializer, MCQAnswerSerializer
-
+from rest_framework.permissions import IsAdminUser
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+from rest_framework import generics
 
 
 class MCQAssignmentViewSet(viewsets.ModelViewSet):
+    queryset = MCQAssignment.objects.all()
+    serializer_class = MCQAssignmentSerializer
+    permission_classes = [IsAdminUser]
+
+    
+@method_decorator(login_required, name='dispatch')
+class MCQAssignmentListAPIView(generics.ListAPIView):
     queryset = MCQAssignment.objects.all()
     serializer_class = MCQAssignmentSerializer
 
 class MCQQuestionViewSet(viewsets.ModelViewSet):
     queryset = MCQQuestion.objects.all()
     serializer_class = MCQQuestionSerializer
+    permission_classes = [IsAdminUser]
 
+    
+@method_decorator(login_required, name='dispatch')
+class MCQQuestionListAPIView(generics.ListAPIView):
+    queryset = MCQQuestion.objects.all()
+    serializer_class = MCQQuestionSerializer
+    
+    
 class OptionViewSet(viewsets.ModelViewSet):
+    queryset = Option.objects.all()
+    serializer_class = OptionSerializer
+    permission_classes = [IsAdminUser]
+
+
+@method_decorator(login_required, name='dispatch')
+class OptionListAPIView(generics.ListAPIView):
     queryset = Option.objects.all()
     serializer_class = OptionSerializer
 
 class MCQSubmissionViewSet(viewsets.ModelViewSet):
     queryset = MCQSubmission.objects.all()
     serializer_class = MCQSubmissionSerializer
+    permission_classes = [IsAdminUser]
+
+    
+@method_decorator(login_required, name='dispatch')
+class MCQSubmissionListAPIView(generics.ListAPIView):
+    queryset = MCQSubmission.objects.all()
+    serializer_class = MCQSubmissionSerializer
 
 class MCQAnswerViewSet(viewsets.ModelViewSet):
+    queryset = MCQAnswer.objects.all()
+    serializer_class = MCQAnswerSerializer
+    permission_classes = [IsAdminUser]
+
+@method_decorator(login_required, name='dispatch')
+class MCQAnswerListAPIView(generics.ListAPIView):
     queryset = MCQAnswer.objects.all()
     serializer_class = MCQAnswerSerializer
