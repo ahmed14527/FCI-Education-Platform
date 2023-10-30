e10e05dea60b004f8ff42f9458ac1af36f06e147
ahmed14527 <108636071+ahmed14527@users.noreply.github.com>
2023-10-30 19:09:24 +0200

    added

--- notification/views.py
+++ notification/views.py
@@ -1,11 +1,22 @@
 from rest_framework import viewsets
 from .models import Notification, Reminder
 from .serializers import NotificationSerializer, ReminderSerializer
+from rest_framework import generics
 
 class NotificationViewSet(viewsets.ModelViewSet):
     queryset = Notification.objects.all()
     serializer_class = NotificationSerializer
 
+
+class NotificationListAPIView(generics.ListAPIView):
+    queryset = Notification.objects.all()
+    serializer_class = NotificationSerializer
+    
+    
 class ReminderViewSet(viewsets.ModelViewSet):
+    queryset = Reminder.objects.all()
+    serializer_class = ReminderSerializer
+    
+class ReminderListAPIView(generics.ListAPIView):
     queryset = Reminder.objects.all()
     serializer_class = ReminderSerializer
