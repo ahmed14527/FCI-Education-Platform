74853c90bb833be4b1347bc8770901ec18de1d90
ahmed14527 <108636071+ahmed14527@users.noreply.github.com>
2023-11-01 06:39:11 +0200

    ss

--- fciplatform/settings.py
+++ fciplatform/settings.py
@@ -97,6 +97,12 @@ DATABASES={
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
     
 }
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    # Other settings...
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
