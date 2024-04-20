889b4056f9a03d83b4c80feadf2929889a52eec7
Bin Shawky <108636071+ahmed14527@users.noreply.github.com>
2024-04-21 01:11:52 +0200

    add

--- fciplatform/settings.py
+++ fciplatform/settings.py
@@ -183,3 +183,13 @@ REST_FRAMEWORK = {
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ]
+}
